b01a23d24bfb9531edd9a78558f79a131689a872
TheRageofGod <spacedwarf1@gmail.com>
2023-03-20 09:47:03 +1100

    Im Not Sure

---
+++ Unity/ShoreTiles/Assets/Scripts/SoldierCard.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SoldierCard : MonoBehaviour, IDragHandler
+{
+    public 
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
+}

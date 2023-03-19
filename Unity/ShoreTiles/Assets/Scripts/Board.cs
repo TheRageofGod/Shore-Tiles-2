ff3267dc28e640b644e5ae8bebe619fd520c36d7
TheRageofGod <spacedwarf1@gmail.com>
2023-03-20 01:35:43 +1100

    Materials

--- Unity/ShoreTiles/Assets/Scripts/Board.cs
+++ Unity/ShoreTiles/Assets/Scripts/Board.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+
+    [Header("Art Stuff")]
+    [SerializeField] private Material tileMaterial;
+
+
     //LOGIC
     private const int TILE_COUNT_X = 6;
     private const int TILE_COUNT_Y = 6;
@@ -32,6 +37,26 @@ public class Board : MonoBehaviour
     {
         GameObject tileObject = new GameObject(string.Format("X:{0}, Y:{1}", x, y));
         tileObject.transform.parent = transform;
+
+        Mesh mesh = new Mesh();
+        tileObject.AddComponent<MeshFilter>().mesh = mesh;
+        tileObject.AddComponent<MeshRenderer>().material = tileMaterial;
+
+
+
+        Vector3[] vertices = new Vector3[4];
+        vertices[0] = new Vector3(x * tileSize, 0, y * tileSize);
+        vertices[1] = new Vector3(x * tileSize, 0, (y+1) * tileSize);
+        vertices[2] = new Vector3((x+1) * tileSize, 0, y * tileSize);
+        vertices[3] = new Vector3((x+1) * tileSize, 0, (y+1) * tileSize);
+
+        int[] tris = new int[] {0, 1, 2, 1, 3, 2};
+
+        mesh.vertices = vertices;
+        mesh.triangles = tris;
+
+        tileObject.AddComponent<BoxCollider>();
+
         return tileObject;
     }
 }

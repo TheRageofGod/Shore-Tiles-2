38c4b316e0cb60ce9c845ec19e22f016db5c5c74
TheRageofGod <spacedwarf1@gmail.com>
2023-03-20 02:06:47 +1100

    Hover

--- Unity/ShoreTiles/Assets/Scripts/Board.cs
+++ Unity/ShoreTiles/Assets/Scripts/Board.cs
@@ -14,13 +14,56 @@ public class Board : MonoBehaviour
     private const int TILE_COUNT_X = 6;
     private const int TILE_COUNT_Y = 6;
     private GameObject[,] tiles;
+    private Camera currentCamera;
+    private Vector2Int currentHover;
+
 
     private void Awake()
     {
-        GenerateAllTiles(1,8,8);
+        GenerateAllTiles(1, TILE_COUNT_X, TILE_COUNT_Y);
     }
 
+    private void Update()
+    {
+        if (!currentCamera)
+        {
+            currentCamera = Camera.main;
+            return;
+        }
 
+        RaycastHit info;
+        Ray ray = currentCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out info, 1000, LayerMask.GetMask("Tile")))
+        {
+            //get Index of selected Tile
+            Vector2Int hitPosition = LookupTileIndex(info.transform.gameObject);
+
+            // If Hovering over a tile after not Hovering
+            if(currentHover == -Vector2Int.one)
+            {
+                currentHover = hitPosition;
+                tiles[hitPosition.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
+            }
+
+            //If already Hovering change Previous selection
+            if (currentHover != -Vector2Int.one)
+            {
+                tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
+                currentHover = hitPosition;
+                tiles[hitPosition.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
+            }
+        }
+        else
+        {
+            if(currentHover != -Vector2Int.one)
+            {
+                tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
+                currentHover = -Vector2Int.one;
+            }
+        }
+    }
+
+    //GenerateBoard
     private void GenerateAllTiles(float tileSize, int tileCountX, int tileCountY)
     {
         tiles = new GameObject[tileCountX, tileCountY];
@@ -54,9 +97,28 @@ public class Board : MonoBehaviour
 
         mesh.vertices = vertices;
         mesh.triangles = tris;
+        mesh.RecalculateNormals();
+
+        tileObject.layer = LayerMask.NameToLayer("Tile");
 
         tileObject.AddComponent<BoxCollider>();
 
         return tileObject;
+    }
+
+    //Operations
+    private Vector2Int LookupTileIndex(GameObject hitInfo)
+    {
+        for (int x = 0; x < TILE_COUNT_X; x++)
+        {
+            for (int y = 0; y < TILE_COUNT_Y; y++)
+            {
+                if (tiles[x, y] == hitInfo)
+                {
+                    return new Vector2Int(x, y);
+                }
+            }
+        }
+        return -Vector2Int.one;//Invalid
     }
 }

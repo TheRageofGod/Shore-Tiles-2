dcbe435856da60d96b92234369c4d6551ea82027
TheRageofGod <spacedwarf1@gmail.com>
2023-03-20 13:43:31 +1100

    Crab Movement

--- Unity/ShoreTiles/Assets/Scripts/CrabController.cs
+++ Unity/ShoreTiles/Assets/Scripts/CrabController.cs
@@ -8,8 +8,18 @@ public class CrabController : MonoBehaviour
     public int Health;
     public int Damage;
     public float movementSpeed = 0.35f;
+    private bool isStopped;
+
     void Update()
     {
+        if(!isStopped)
         transform.Translate(new Vector3(movementSpeed * 0, 1, 0));
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 10)
+        {
+            isStopped = true;
+        }
     }
 }

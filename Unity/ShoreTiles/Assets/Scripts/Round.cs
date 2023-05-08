9f9e1aab51799af9e3891fffaa6a3b6d16bcf033
TheRageofGod <spacedwarf1@gmail.com>
2023-05-08 11:00:01 +1000

    Wave

--- Unity/ShoreTiles/Assets/Scripts/Round.cs
+++ Unity/ShoreTiles/Assets/Scripts/Round.cs
@@ -6,9 +6,11 @@ public class Round : MonoBehaviour
 {
     int round = 1;
     public Phase phase;
+    public GameManager Gm;
     public void NextRound()
     {
         round = round + 1;
         phase = Phase.DEPLOY;
+        Gm.RoundCounter = round;
     }
 }
